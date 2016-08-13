20566305a84a31dd92c4739f1ff835e865b3a2db
Alex Jeffreys <alexjeffreys@Alexs-MacBook-Pro.local>
2016-08-13 17:25:32 +0100

    Set window to fixed size

--- codenames_app.py
+++ codenames_app.py
@@ -3,10 +3,15 @@ kivy.require('1.9.1')
 
 from kivy.app import App
 from codenames_game import CodenamesGame
+from kivy.config import Config
+Config.set('graphics', 'width', 1000)
+Config.set('graphics', 'height', 750)
 
 class CodenamesApp(App):
+
     def build(self):
        game = CodenamesGame()
+       game.play()
        return game
 
 if __name__ == "__main__":
